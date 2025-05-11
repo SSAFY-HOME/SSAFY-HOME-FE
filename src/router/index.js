7d18836d7075febc7055c48f9577aaf9853c0fbf
mkwak1125 <mkwak1125@gmail.com>
2025-05-11 22:20:31 +0900

    feat : 메인화면 지도 띄우기, 패널 구현

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       name: 'signup',
       component: () => import('../views/SignupView.vue'),
     },
+    {
+      path: '/main',
+      name: 'main',
+      component: () => import('../views/MapView.vue'),
+    },
   ],
 })
 
